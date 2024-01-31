be423f448c78b3e832c77811614c53a2cb843cf7
RISHI RAJ <rishiraj@gmail.com>
2024-01-31 23:01:32 +0530

    code changed

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -3,4 +3,5 @@ export const BACKEND_URL = 'https://quizappbackend-jpws.onrender.com';
 
 
 // export const FRONTEND_URL = 'http://localhost:3000';
-export const FRONTEND_URL = 'https://quiz-appp12.netlify.app';
+// export const FRONTEND_URL = 'https://quiz-appp12.netlify.app';
+export const FRONTEND_URL = 'https://quiz-appp12.vercel.app';
