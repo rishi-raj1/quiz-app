2e45c612e0e2f76a1f1b38af3d37cd228af804e4
RISHI RAJ <rishiraj@gmail.com>
2024-01-31 21:39:38 +0530

    changed backend url

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,3 +1,5 @@
-export const BACKEND_URL = 'http://localhost:4000';
+// export const BACKEND_URL = 'http://localhost:4000';
+export const BACKEND_URL = 'https://quizappbackend-jpws.onrender.com';
+
 
 export const FRONTEND_URL = 'http://localhost:3000';
